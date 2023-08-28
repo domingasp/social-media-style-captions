fdef274618be8261cccbfaeeb43e773cb3a215f6
Domingas Pleckauskas <d.pleck@outlook.com>
2023-08-28 22:05:10 +0100

    logic for batching lines to similar lengths

--- src/views/CaptionCreator.tsx
+++ src/views/CaptionCreator.tsx
@@ -1,23 +1,53 @@
 import { Box, Button, Stack, Text, Textarea, Title } from "@mantine/core";
-import { toPng, toSvg } from "html-to-image";
+import { toSvg } from "html-to-image";
 import { useEffect, useState } from "react";
+
+const getTextWidth = function getTextWidth(text: string) {
+  const canvas: HTMLCanvasElement = document.createElement("canvas");
+  const context = canvas.getContext("2d");
+  context!.font = "600 Nunito Sans sans-serif";
+  const metrics = context!.measureText(text);
+  return metrics.width;
+};
+
+const isSimilarLength = function isSimilarLength(
+  text: string,
+  nextText: string
+) {
+  return Math.abs(getTextWidth(text) - getTextWidth(nextText)) < 7.5;
+};
+
+const isShorter = function isShorter(text: string, textToCompare: string) {
+  return getTextWidth(text) < getTextWidth(textToCompare);
+};
 
 function CaptionCreator() {
   const radius = "9px";
 
   const [content, setContent] = useState("Text\nTesting");
   const [lines, setLines] = useState<string[]>([]);
+  const [batchedLines, setBatchedLines] = useState<string[][]>([]);
 
   useEffect(() => {
-    setLines(content.split("\n"));
-  }, [content]);
+    const contentSplit = content.split("\n");
+    setLines(contentSplit);
 
-  const getTextWidth = function getTextWidth(text: string) {
-    const canvas: HTMLCanvasElement = document.createElement("canvas");
-    const context = canvas.getContext("2d");
-    const metrics = context!.measureText(text);
-    return metrics.width;
-  };
+    const batched: string[][] = [[contentSplit[0]]];
+    let batch = 0;
+    for (let i = 0; i < contentSplit.length - 1; i += 1) {
+      const curr = contentSplit[i];
+      const next = contentSplit[i + 1];
+
+      if (!isSimilarLength(curr, next)) {
+        batch += 1;
+        if (batched.length === batch) batched.push([]);
+      }
+
+      batched[batch].push(next);
+    }
+
+    setBatchedLines(batched);
+  }, [content]);
 
   const getOuterRadius = function getOuterRadius(
     radiusPos: string = "full-left",
@@ -106,33 +136,105 @@ function CaptionCreator() {
       </Button>
 
       <Stack align="center" spacing={0} id="output" p="10px 30px">
-        {lines.map((line, i) => (
-          <Box
-            key={i}
-            sx={{
-              backgroundColor: "red",
-              padding: "1rem 1.1rem 0.75rem 1.1rem",
-              position: "relative",
-              marginTop: "-1px",
-              borderTopLeftRadius: radius,
-              borderTopRightRadius: radius,
-              borderBottomLeftRadius: radius,
-              borderBottomRightRadius: radius,
-            }}
-          >
-            {getOuterRadius(`top-left`, "right")}
-            {getOuterRadius(`bottom-right`, "left")}
-            <Text
-              className="tiktok-classic-text"
-              size="2rem"
-              sx={{
-                lineHeight: "1.75rem",
-              }}
-            >
-              {line}
-            </Text>
-          </Box>
-        ))}
+        {batchedLines.map((batch, i) => {
+          const isOnlyBatch = batchedLines.length === 1;
+          const isFirstBatch = i === 0;
+          const isLastBatch = i === batchedLines.length - 1;
+          const isSandwichedBatch = !isFirstBatch && !isLastBatch;
+
+          return (
+            <Box key={i}>
+              {batch.map((line, j) => {
+                const isOnly = batch.length === 1;
+                const isFirst = j === 0;
+                const isLast = j === batch.length - 1;
+                const isSandwiched = !isFirst && !isLast;
+
+                let topLeftRadius,
+                  topRightRadius,
+                  bottomLeftRadius,
+                  bottomRightRadius;
+                topLeftRadius =
+                  topRightRadius =
+                  bottomLeftRadius =
+                  bottomRightRadius =
+                    "0px";
+
+                if (isOnlyBatch) {
+                  if (isFirst) topLeftRadius = topRightRadius = radius;
+                  if (isLast) bottomLeftRadius = bottomRightRadius = radius;
+                } else if (isFirstBatch) {
+                  if (isFirst) topLeftRadius = topRightRadius = radius;
+                  if (isLast) {
+                    bottomLeftRadius = bottomRightRadius = isShorter(
+                      line,
+                      batchedLines[i + 1][0]
+                    )
+                      ? "0px"
+                      : radius;
+                  }
+                } else if (isLastBatch) {
+                  if (isLast) bottomLeftRadius = bottomRightRadius = radius;
+                  if (isFirst && !isOnly) {
+                    topLeftRadius = topRightRadius = isShorter(
+                      line,
+                      batchedLines[i - 1].at(-1)!
+                    )
+                      ? "0px"
+                      : radius;
+                  }
+                } else {
+                  if (isFirst) {
+                    topLeftRadius = topRightRadius = isShorter(
+                      line,
+                      batchedLines[i - 1].at(-1)!
+                    )
+                      ? "0px"
+                      : radius;
+                  }
+                  if (isLast) {
+                    bottomLeftRadius = bottomRightRadius = isShorter(
+                      line,
+                      batchedLines[i + 1][0]
+                    )
+                      ? "0px"
+                      : radius;
+                  }
+                }
+
+                return (
+                  <Box
+                    key={j}
+                    sx={{
+                      backgroundColor: "red",
+                      padding: "1rem 1.1rem 0.75rem 1.1rem",
+                      position: "relative",
+                      marginTop: "-1px",
+                      borderTopLeftRadius: topLeftRadius,
+                      borderTopRightRadius: topRightRadius,
+                      borderBottomLeftRadius: bottomLeftRadius,
+                      borderBottomRightRadius: bottomRightRadius,
+                      width: "100%",
+                      textAlign: "center",
+                    }}
+                  >
+                    {/* {getOuterRadius(`top-left`, "right")}
+                    {getOuterRadius(`bottom-right`, "left")} */}
+                    <Text
+                      className="tiktok-classic-text"
+                      size="2rem"
+                      sx={{
+                        lineHeight: "1.75rem",
+                      }}
+                    >
+                      {line}
+                    </Text>
+                  </Box>
+                );
+              })}
+            </Box>
+          );
+        })}
       </Stack>
     </Stack>
   );
